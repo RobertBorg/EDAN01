d5e77e4e24615fc469f361742f42eb29874b8ed1
Richard Simko <richard.simko@icloud.com>
2014-02-12 13:09:21 +0100

    Begun lab 3 impl

---
+++ src/lab3/Jobshop.java
@@ -0,0 +1,46 @@
+package lab3;
+
+import org.jacop.constraints.Cumulative;
+import org.jacop.constraints.XplusClteqZ;
+import org.jacop.core.IntVar;
+import org.jacop.core.Store;
+
+public class Jobshop {
+	public static void main(String[] args) {
+		long T1, T2, T;
+		T1 = System.currentTimeMillis();
+		doWork();
+		T2 = System.currentTimeMillis();
+		T = T2 - T1;
+		System.out.println("\n\t*** Execution time = " + T + " ms");
+	}
+
+	/**
+	 * This does work
+	 */
+	public static void doWork() {
+		Store store = new Store();
+		IntVar startTime11 = new IntVar(store, "Job 1 - Task 1 - S", 0, 100);
+		IntVar startTime12 = new IntVar(store, "Job 1 - Task 2 - S", 1, 100);
+		store.impose(new XplusClteqZ(startTime11, 1, startTime12)); //And so on...
+		IntVar startTime13 = new IntVar(store, "Job 1 - Task 3 - S", 4, 100);
+		IntVar startTime14 = new IntVar(store, "Job 1 - Task 4 - S", 10, 100);
+		IntVar startTime15 = new IntVar(store, "Job 1 - Task 5 - S", 17, 100);
+		IntVar startTime16 = new IntVar(store, "Job 1 - Task 6 - S", 20, 100);
+		//Make similar varables for all jobs
+		
+		IntVar[] durationsMachine0 = { new IntVar(store, 3, 3), new IntVar(store, 10, 10), new IntVar(store, 9, 9),
+				new IntVar(store, 5, 5), new IntVar(store, 3, 3), new IntVar(store, 10, 10) };
+		IntVar[] durationsMachine1 = { new IntVar(store, 6, 6), new IntVar(store, 8, 8), new IntVar(store, 1, 1), new IntVar(store, 5, 5),
+				new IntVar(store, 3, 3), new IntVar(store, 3, 3) };
+		IntVar[] durationsMachine2 = { new IntVar(store, 1, 1), new IntVar(store, 5, 5), new IntVar(store, 5, 5), new IntVar(store, 5, 5),
+				new IntVar(store, 9, 9), new IntVar(store, 1, 1) };
+		IntVar[] durationsMachine3 = { new IntVar(store, 7, 7), new IntVar(store, 4, 4), new IntVar(store, 4, 4), new IntVar(store, 3, 3),
+				new IntVar(store, 1, 1), new IntVar(store, 3, 3) };
+		IntVar[] durationsMachine4 = { new IntVar(store, 6, 6), new IntVar(store, 10, 10), new IntVar(store, 7, 7), new IntVar(store, 8, 8),
+				new IntVar(store, 5, 5), new IntVar(store, 4, 4) };
+		IntVar[] durationsMachine5 = { new IntVar(store, 3, 3), new IntVar(store, 10, 10), new IntVar(store, 8, 8), new IntVar(store, 9, 9),
+				new IntVar(store, 4, 4), new IntVar(store, 9, 9) };
+		// Cumulative cum = new Cumulative(starts, durations, resources, limit)
+	}
+}
